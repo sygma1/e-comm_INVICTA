68341d8f73dfe4930d00aee93625abc522a58b76
sygma1 <brmammar@gmail.com>
2024-12-01 18:13:54 +0100

    changed switch to route

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import 'bootstrap-icons/font/bootstrap-icons.css'; // Import Bootstrap Icons CSS
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import ProductList from './components/ProductList';
 import ProductDetail from './components/ProductDetail';
 import Register from './components/Register';
@@ -25,14 +25,14 @@ const Main = () => {
     <div>
       {/* Conditionally render Header only if the path is not login or register */}
       {location.pathname !== '/login' && location.pathname !== '/register' && <Header />}
-      <Switch>
-        <Route path="/login" exact render={() => <Login />} />
-        <Route path="/register" exact render={() => <Register />} />
-        <Route path="/productlist" exact render={() => <ProductList />} />
-        <Route path="/product/:id" exact render={() => <ProductDetail />} />
-        <Route path="/cart" exact render={() => <Cart />} />
-        <Route path="/checkout" exact render={() => <Checkout />} />
-      </Switch>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
     </div>
   );
 };
